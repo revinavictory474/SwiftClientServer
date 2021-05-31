12608cf2984afd8c7a475afa1a77c01e5450ce11
catostropher <catostropher@gmail.com>
2021-06-01 00:45:35 +0700

    Lesson2

--- someNewProject/Controllers/MainViewController.swift
+++ someNewProject/Controllers/MainViewController.swift
@@ -6,72 +6,95 @@
 //
 
 import UIKit
+import WebKit
+import Alamofire
 
 class MainViewController: UIViewController {
     let mySingleton = MySingleton.instance
-//    let mySingletonPlus = MySingleton()
     private let notifactionName = Notification.Name(NotificationNames.someNotification.rawValue)
     
-    @IBOutlet weak var userNameTextField: UITextField!
-    @IBOutlet weak var userSurnameTextField: UITextField!
-    @IBAction func saveButtonAction(_ sender: Any) {
-        mySingleton.userName = userNameTextField.text ?? ""
-        mySingleton.userSurname = userSurnameTextField.text ?? ""
+  @IBOutlet weak var webview: WKWebView! {
+    didSet{
+      webview.navigationDelegate = self
     }
-    @IBAction func goToSecondAction(_ sender: Any) {
-        guard let secondVC = UIStoryboard(name: "Main", bundle: nil)
-                .instantiateViewController(identifier: "SecondViewController")
-                as? SecondViewController else { return }
-        secondVC.delegate = self
-        
-        present(secondVC,
-                animated: true)
-    }
+  }
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        userSurnameTextField.delegate = self
-        
-        NotificationCenter
-            .default
-            .addObserver(
-                self,
-                selector: #selector(notificationRecive(notification:)),
-                name: notifactionName,
-                object: nil)
-    }
+  override func viewDidLoad() {
+      super.viewDidLoad()
+      var urlComponents = URLComponents()
+      urlComponents.scheme = "https"
+      urlComponents.host = "oauth.vk.com"
+      urlComponents.path = "/authorize"
+      urlComponents.queryItems = [
+        URLQueryItem(name: "client_id", value: "7868515"),
+        URLQueryItem(name: "display", value: "mobile"),
+        URLQueryItem(name: "redirect_uri", value: "https://oauth.vk.com/blank.html"),
+        URLQueryItem(name: "scope", value: "262150"),
+        URLQueryItem(name: "response_type", value: "token"),
+        URLQueryItem(name: "v", value: "5.68")
+      ]
+    let request = URLRequest(url: urlComponents.url!)
+    webview.load(request)
     
-    deinit {
-        NotificationCenter.default.removeObserver(
-            self,
-            name: notifactionName,
-            object: nil)
-    }
+  }
 
-    
-    @objc private func notificationRecive(notification: Notification) {
-        self.view.backgroundColor = notification.userInfo?["color"] as? UIColor
-    }
-    
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-        userNameTextField.text = mySingleton.userName
-        userSurnameTextField.text = mySingleton.userSurname
-    }
-    
-    
+  override func viewDidAppear(_ animated: Bool) {
+      super.viewDidAppear(animated)
+  }
 }
 
-extension MainViewController: MyNewDelegate {
-    func didPressButton() {
-        mySingleton.userName = ""
-        mySingleton.userSurname = ""
-    }
+extension MainViewController:WKNavigationDelegate {
+  func webView(_ webView: WKWebView,
+               decidePolicyFor navigationResponse: WKNavigationResponse,
+               decisionHandler: @escaping (WKNavigationResponsePolicy) -> Void) {
+                  guard let url = navigationResponse.response.url, url.path == "/blank.html", let fragment = url.fragment  else {            decisionHandler(.allow)
+                  return
+              }
+    let params = fragment
+      .components(separatedBy: "&")
+      .map { $0.components(separatedBy: "=") }
+      .reduce([String: String]()) { result, param in
+        var dict = result
+        let key = param[0]
+        let value = param[1]
+        dict[key] = value
+        return dict
+      }
+    Session.instance.token = params["access_token"] ?? ""
+    print("Token is \(Session.instance.token)")
+    
+    var parameters: Parameters = [
+      "access_token": Session.instance.token,
+      "v": "5.68"
+    ]
+    Makerequest(apiMethod: "apps.getFriendsList", parameters: parameters)
+    
+    parameters = [
+      "access_token": Session.instance.token,
+      "v": "5.68",
+      "owner_id": "170575832"
+    ]
+    Makerequest(apiMethod: "photos.getAll", parameters: parameters)
+    
+    parameters = [
+      "access_token": Session.instance.token,
+      "v": "5.68",
+      "user_id": "170575832"
+    ]
+    Makerequest(apiMethod: "groups.get", parameters: parameters)
+    
+    parameters = [
+      "access_token": Session.instance.token,
+      "v": "5.68",
+      "q": "Swift"
+    ]
+    Makerequest(apiMethod: "groups.search", parameters: parameters)
+      
+    decisionHandler(.cancel)
+  }
+  
+  func Makerequest(apiMethod: String, parameters: Parameters) {
+    AF.request("https://api.vk.com/method/" + apiMethod, method: .get, parameters: parameters)
+      .responseJSON { response in print("\(apiMethod) is \(response.value)") }
+  }
 }
-
-extension MainViewController: UITextFieldDelegate {
-    func textFieldDidBeginEditing(_ textField: UITextField) {
-        print("StartEditing")
-    }
-}
-
